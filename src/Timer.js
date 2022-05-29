5a328455f80007967891b092882a79044929d508
RamitD <rdihidar1997@gmail.com>
2022-05-29 10:41:42 +0530

    adding timer

---
+++ src/Timer.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Timer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            clock: 0
+        }
+        this.ticker=this.ticker.bind(this)
+    }
+
+    componentDidMount(){
+        setInterval(this.ticker,1000);
+    }
+
+    ticker(){
+        this.setState({clock: new Date() - this.props.start})
+    }
+
+    render() {
+        var clock =Math.round(this.state.clock/1000);
+        return (
+            <div>
+                <p>Ypu have been on this site for :</p>
+                <span className="seconds">{clock}</span>
+                <p>seconds</p>
+            </div>
+        );
+    }
+}
+
+export default Timer;
